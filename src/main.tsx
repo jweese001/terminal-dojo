bdc42a3b8ebb7b5778c38917dbec0af1aac3ed0e
jweese001 <jweese@bioscapedigital.com>
2026-03-02 02:20:39 -0500

    Add dojo background image

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
 
+const baseUrl = import.meta.env.BASE_URL;
+document.documentElement.style.setProperty('--bg-image', `url('${baseUrl}images/dojo.webp')`);
 createRoot(document.getElementById('root')!).render(<App />);
